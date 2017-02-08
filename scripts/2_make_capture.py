b355b15bccb39d9df38ca2fb0a56ad51cea5d453
Mathy <vanhoefm@users.noreply.github.com>
2017-02-08 22:13:38 +0100

    scripts: give bssid as argument to 2_make_capture.py

--- scripts/2_make_capture.py
+++ scripts/2_make_capture.py
@@ -5,15 +5,19 @@ import sys
 got_beacon = False
 got_data = False
 capture = []
+mac_router = None
 
 def process_packet(p):
+	global mac_router
 	global got_beacon
 	global got_data
 	global capture
 
+	print mac_router
+
 	if p.addr1 != "ff:ff:ff:ff:ff:ff":
 		return
-	if p.addr2 != "38:2c:4a:c1:69:bc":
+	if p.addr2 != mac_router:
 		return
 
 	if not got_beacon and Dot11Beacon in p:
@@ -38,16 +42,18 @@ def main():
 	But this scripts let's you know wheb a Beacon and Broadcast Data frame has been captured,
 	and then automatically saves the capture and terminates.
 	"""
+	global mac_router
 	global got_beacon
 	global got_data
 	global capture
 
-	if len(sys.argv) != 2:
-		print "Usage: %s interface" % sys.argv[0]
+	if len(sys.argv) != 3:
+		print "Usage: %s interface macrouter" % sys.argv[0]
 		quit(1)
 
 	print "Capturing frames ..."
 	conf.iface = sys.argv[1]
+	mac_router = sys.argv[2]
 	sniff(iface=conf.iface, prn=process_packet, stop_filter= lambda p: got_beacon and got_data)
 
 	capname = "capture.pcap"
